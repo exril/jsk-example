5c4d7e8e2c5cbef2db59d59ce5cccedbae88be5f
Shubham <143504262+ftpskid@users.noreply.github.com>
2024-02-06 08:07:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import discord
 from discord.ext import commands
 from jishaku.cog import Jishaku
-import tracemalloc  # Import tracemalloc
+import tracemalloc  
 
-# Enable tracemalloc
 tracemalloc.start()
 
-# Create an instance of the bot
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Event to print a message when the bot is ready
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print(f'Logged in as {bot.user.name}')
     print('------')
     await bot.change_presence(activity=discord.Game(name="awarebot.pro/invite"))
-    await bot.wait_until_ready()  # Ensure bot is fully ready before loading extensions
-    await bot.load_extension('jishaku')  # Load the Jishaku extension
+    await bot.wait_until_ready() 
+    await bot.load_extension('jishaku') 
+bot.owner_ids = {847770840266833961} 
 
-# Set the bot owner(s) ID(s)
-bot.owner_ids = {847770840266833961}  # Replace with your owner ID(s)
-
-# Simple command to respond with a greeting
-@bot.command(name='hello', help='Responds with a greeting')
+@bot.command(name='help', help='Responds with a greeting')
 async def hello(ctx):
-    await ctx.send(f'Hello, {ctx.author.mention}!')
+    await ctx.send(f'https://awarebot.pro/invite')
 
-# Run the bot with your token
 bot.run('MTIwNDIzMzMwNjg5MjQwMjY5OA.G3XaQp.9JccFarIEzWBcdC0ol0eNxC2fwSwZ7JUNfXRx0')
