65f1b2082c2da108808a483a2ca005b26797ea1e
Shubham <143504262+ftpskid@users.noreply.github.com>
2024-02-06 06:51:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands, tasks
+from jishaku.cog import Jishaku  # Import the Jishaku cog
+
+# Create an instance of the bot
+bot = commands.Bot(command_prefix='a!')
+bot.owner_ids = [847770840266833961]
+
+# Load the Jishaku extension
+bot.load_extension('jishaku')
+
+# Event to print a message when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print('------')
+
+# Simple command to respond with a greeting
+@bot.command(name='hello', help='Responds with a greeting')
+async def hello(ctx):
+    await ctx.send(f'Hello, {ctx.author.mention}!')
+
+# Run the bot with your token
+bot.run('YOUR_BOT_TOKEN')
